9fc110472d30d064f15197f589c602df8c1174b0
vincent <vramouss@912e105-08.nfc.univ-fcomte.fr>
2019-02-06 09:24:58 +0100

    link classe

---
+++ Code/ios/AppliCours/AppliCours/EleveTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EleveTableViewCell.swift
+//  AppliCours
+//
+//  Created by ramousset vincent on 06/02/2019.
+//  Copyright © 2019 ramousset vincent. All rights reserved.
+//
+
+import UIKit
+
+class EleveTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Code/ios/AppliCours/AppliCours/EleveViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EleveViewController.swift
+//  AppliCours
+//
+//  Created by ramousset vincent on 06/02/2019.
+//  Copyright © 2019 ramousset vincent. All rights reserved.
+//
+
+import UIKit
+
+class EleveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
